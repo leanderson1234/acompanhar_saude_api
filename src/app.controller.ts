a67e84f6cf585484a3336110fec706c536f42cb8
Leanderson de Oliveira Santana <losantana@uneb.br>
2025-02-10 17:43:25 -0300

    config inicial jwt

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,30 @@
-import { Controller, Get } from '@nestjs/common';
+
+import { Controller, Request, Post, UseGuards, Get } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { AppService } from './app.service';
+import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthService } from './auth/auth.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService,private authService:AuthService) {}
+
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
+  
+@UseGuards(LocalAuthGuard)
+@Post('auth/logout')
+async logout(@Request() req) {
+  return req.logout();
 }
+
+}
+
